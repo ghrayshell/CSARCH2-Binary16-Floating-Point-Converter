e8224b6335cb8d643259ceba23987e61197ea24f
carlosgutierrez121 <134764909+carlosgutierrez121@users.noreply.github.com>
2024-03-22 04:30:38 +0800

    AddedGenerateOutputForBinary

--- javascript/script.js
+++ javascript/script.js
@@ -25,21 +25,48 @@ function getInputFormat() {
 function getExponent() {
     var exp = document.getElementById("expBox").value;
 
-    if(exp < 0)  {
-        /* ZERO */
+    const expPrime = getExpPrime(parseInt(exp));
+    const binary = decToBinary(expPrime);
+
+    return binary;
+
+    // if(exp < 0)  {
+    //     /* ZERO */
+    // }
+    // else if(exp > 0) {
+    //     /* DENORMALIZE */
+    // }
+    // else if(exp > 0) {
+    //     /* INFINITY */
+    // }
+    // else if(exp > 0) {
+    //     /* NaN - Not a Number */
+    // }
+    // else {
+    //     /* Normal Case */
+    // }
+}
+
+function getExpPrime(exp) {
+    return exp + 15;
+}
+
+function isNegative(mantissa){
+    if(mantissa.charAt(0) === "-"){
+        return "1"
+    } else{
+        return "0"
     }
-    else if(exp > 0) {
-        /* DENORMALIZE */
-    }
-    else if(exp > 0) {
-        /* INFINITY */
-    }
-    else if(exp > 0) {
-        /* NaN - Not a Number */
-    }
-    else {
-        /* Normal Case */
-    }
+}
+
+function generateOutput(mantissa, exp){
+    const flag = isNegative(mantissa);
+    const exponent_bits = getExponent();
+    const fraction_bits = mantissa.substring(3);
+
+    const output = flag + exponent_bits + fraction_bits;
+
+    console.log(output);
 }
 
 function resetValues() {
@@ -47,6 +74,31 @@ function resetValues() {
     document.getElementById("inputBox").value = "";
     document.getElementById("expBox").value = "";
     getInputFormat();
+}
+
+function decToBinary(input){
+    output = "";
+
+    var val = parseInt(input);
+
+    while(val > 0){
+        if(val % 2 == 1) {
+            output = "1" + output;
+        } else {
+            output = "0" + output;
+        }
+
+        val = Math.floor(val / 2);
+    }
+
+    return output;
+}
+
+function handleConvert() {
+    const mantissa = document.getElementById("inputBox").value;
+    const exp = document.getElementById("expBox").value;
+
+    generateOutput(mantissa, exp);
 }
 
 function convertBinary() {
